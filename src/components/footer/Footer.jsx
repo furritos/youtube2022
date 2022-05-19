26e0c5efaab7c4a934aa731196c1ae81bd580b0f
Carlos <carlos@riv.as>
2022-05-18 22:18:39 -0700

    Footer page

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import "./footer.scss"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="fLists">
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airport</li>
+                    <li className="fListItem">Hotes</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airport</li>
+                    <li className="fListItem">Hotes</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airport</li>
+                    <li className="fListItem">Hotes</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airport</li>
+                    <li className="fListItem">Hotes</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airport</li>
+                    <li className="fListItem">Hotes</li>
+                </ul>
+            </div>
+            <div className="fText">Copyright © 2022 - lamabooking</div>
+        </div>
+    )
+}
+
+export default Footer
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,8 @@ import Navbar from '../../components/navbar/Navbar'
 import Featured from '../../components/featured/Featured'
 import PropertyList from '../../components/propertyList/PropertyList'
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
+import MailList from '../../components/mailList/MailList'
+import Footer from '../../components/footer/Footer'
 import "./home.scss"
 
 const Home = () => {
@@ -17,6 +19,8 @@ const Home = () => {
                 <PropertyList />
                 <h1 className="homeTitle">Homes guests love</h1>
                 <FeaturedProperties />
+                <MailList />
+                <Footer />
             </div>
         </div>
     )
